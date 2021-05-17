a4a127f19e8abb6e0612de5e5b184b17fde6b214
unknown <santiago.guzman2003@gmail.com>
2021-05-17 09:19:56 +0200

    add language pages

--- docs/script.js
+++ docs/script.js
@@ -35,3 +35,26 @@ document.addEventListener('DOMContentLoaded', () => {
   
     });
   });
+
+  //Search Bar
+  function mySearchFunction() {
+    var input, filter, ul, li, item, i, txtValue;
+    input = document.getElementById("myInput");
+
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("Kurser");
+    li = ul.getElementsByTagName("li");
+
+    for (i = 0; i < li.length; i++) {
+        item = li[i];
+
+        txtValue = item.textContent || item.innerText;
+
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
