94aa4676f79c8c735787d26399b2fac28ce08062
Santiago Guzman <sanguz@depinf.local>
2021-05-20 08:27:13 +0200

    script edit

--- Comeet.sh
+++ Comeet.sh
@@ -5,6 +5,14 @@ function comeet(){
   let proc=6
   trash="/dev/null"
   current_user=$(whoami)
+  # Cool text.
+  echo " ██████╗ ██████╗ ███╗   ███╗███████╗███████╗████████╗"
+  echo "██╔════╝██╔═══██╗████╗ ████║██╔════╝██╔════╝╚══██╔══╝"
+  echo "██║     ██║   ██║██╔████╔██║█████╗  █████╗     ██║   "
+  echo "██║     ██║   ██║██║╚██╔╝██║██╔══╝  ██╔══╝     ██║   "
+  echo "╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗███████╗   ██║   "
+  echo " ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝╚══════╝   ╚═╝   "
+
   # Introductory text.
   echo "================================================================================"
   echo -e " Welcome \e[1;34m"$current_user"\e[0m - to the bash script installation of the  "
@@ -16,7 +24,6 @@ function comeet(){
   echo
   echo "Remaining processes:"$proc
   # Tasks.
-  echo "Set a domain" read DOMAIN 
   #
   echo "Mounting the docker container"
   docker build - t jitsi-- build - arg DOMAIN = $DOMAIN.docker run - p 443: 443 / tcp - p 80: 80 / tcp - p 10000: 10000 / udp - p 3478: 3478 / udp - p 5349:5349 / tcp-- name contenedor - jitsi - d jitsi &> $trash #
@@ -40,10 +47,6 @@ function comeet(){
   #
   echo "Starting the jicofo service in the container"
   docker exec - it contenedor - jitsi service jicofo start &> $trash #
-  let "proc -= 1"
-  echo "Remaining processes: "$proc
-  #
-  apt-get autoremove -y &> $s_null #
   # We indicate to the user that we have completed the tasks.
   echo
   echo "[Tasks completed successfully]"
@@ -55,6 +58,17 @@ function comeet(){
 }
 # We clean the terminal of previously executed commands.
 clear
+# We verify that the docker package is installed.
+# In the case of FALSE, we install the docker.io package
+if !dpkg -s docker.io &> $trash; then
+echo 
+echo "============================================================================"
+echo "¡The docker package is not currently installed!" 1>&2
+echo "============================================================================"
+echo
+echo "[installing the package]"
+sudo apt install - y docker.io 
+fi
 # We check that the Bash file has been run as SuperUser - root.
 # In the case of FALSE, we inform the user to rerun with root permissions.
 # In case of TRUE, the comeet function is called.
@@ -66,6 +80,7 @@ if [ "$(id -u)" != "0" ]; then
    echo
     exit 1
 else
+  echo "Set a domain" read DOMAIN 
   comeet
     exit 1
 fi
