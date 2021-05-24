51c4e7284313c53799aa49e008fd536a75357369
SantitoGit <68120901+SantitoGit@users.noreply.github.com>
2021-05-24 21:27:14 +0200

    Update delete.php

--- conf/confpage/delete.php
+++ conf/confpage/delete.php
@@ -6,9 +6,9 @@ $dominio = $_SERVER['HTTP_HOST'];
 $rutaSalas = str_replace('.', '%2e', $dominio);
 $rutaSalas = '/var/lib/prosody/conference%2e'.$rutaSalas.'/config';
 $archivo = $rutaSalas.'/'.$sala.'.dat';
-shell_exec('sudo /etc/init.d/prosody stop');
+//shell_exec('sudo /etc/init.d/prosody stop');
 shell_exec('rm '.$archivo);
-shell_exec('sudo /etc/init.d/prosody start');
+//shell_exec('sudo /etc/init.d/prosody start');
 shell_exec('bash /usr/share/jitsi-meet/confpage/chdat.sh');
 echo $sala." was succefully deleted. It make take some seconds for rooms to start working again.";
 }
